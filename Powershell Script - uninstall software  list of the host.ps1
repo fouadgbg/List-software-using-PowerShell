5653dfd084e913335e24d9df8bae5958a3664769
fouadgbg <74716591+fouadgbg@users.noreply.github.com>
2021-04-23 21:21:05 +0100

    Add files via upload

---
+++ Powershell Script - uninstall software  list of the host.ps1
@@ -0,0 +1,6 @@
+﻿$servers = Get-Content c:\extract\server.txt
+
+foreach( $server in $servers){
+$app = Get-WmiObject Win32_product -ComputerName $server | where {$_.name -eq "Adobe Acrobat Reader DC" }
+$app.Uninstall()
+}
---
+++ Powershell script - installed software list of the host.ps1
@@ -0,0 +1,11 @@
+﻿Get-Content c:\extract\server.txt
+# Software Installed
+#Variable $servers
+
+$results= foreach( $server in $servers){
+
+Write-Host "Processing Server $server"
+
+Get-WmiObject -Class Win32_product -ComputerName $server | select __SERVER, Name, Version -ErrorAction SilentlyContinue
+}
+$results | export-Csv -Path C:\extract\software.csv
